c423303424c36277b963091ca83e5c1a166520e2
phayes <patrick.d.hayes@gmail.com>
2016-04-04 08:51:48 -0700

    3161 specific identifiers

--- rfc3161.go
+++ rfc3161.go
@@ -8,6 +8,15 @@ import (
 // Set InitMimeTypes to initialize mimetimes for timestamp-query and timestamp-reply
 var InitMimeTypes bool
 
+// OID Identifiers
+var (
+	// RFC-2630: iso(1) member-body(2) us(840) rsadsi(113549) pkcs(1) pkcs7(7) 2
+	OidSignedData = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 7, 2}
+
+	// RFC-3161: iso(1) member-body(2) us(840) rsadsi(113549) pkcs(1) pkcs-9(9) smime(16) ct(1) 4
+	OidContentTypeTSTInfo = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 9, 16, 1, 4}
+)
+
 func SetMimeTypes() error {
 	err := mime.AddExtensionType(".tsq", "application/timestamp-query")
 	if err != nil {
